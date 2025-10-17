2c236d970e844cfdc8f3289c75330bae46775de5
Aditya Nandan <nandanaditya985@gmail.com>
2025-10-17 16:50:02 +0530

    keyboard controls

--- src/components/ui/Tab.vue
+++ src/components/ui/Tab.vue
@@ -2,36 +2,47 @@
 import { ref } from 'vue'
 
 const selected = ref(0)
-defineProps<{ data: { label: string; content: string }[] }>()
+const props = defineProps<{ data: { label: string; content: string }[] }>()
+
+const handleKeydown = (e: KeyboardEvent) => {
+  if (e.key === 'ArrowRight') {
+    selected.value = (selected.value + 1) % props.data.length
+  }
+  if (e.key === 'ArrowLeft') {
+    selected.value = (selected.value - 1 + props.data.length) % props.data.length
+  }
+}
 </script>
 
 <template>
-  <div role="tablist" class="border-b border-gray-300">
-    <button
-      class="px-5 py-2 bg-gray-300 cursor-pointer"
-      role="tab"
-      @click="selected = i"
-      :style="{
-        borderBottom: selected === i ? '2px solid #3293f4' : '2px solid #d1d5dc',
-        backgroundColor: selected === i ? 'white' : '#d1d5dc',
-      }"
+  <div @keydown="handleKeydown">
+    <div role="tablist" class="border-b border-gray-300">
+      <button
+        class="px-5 py-2 bg-gray-300 cursor-pointer"
+        role="tab"
+        @click="selected = i"
+        :style="{
+          borderBottom: selected === i ? '2px solid #3293f4' : '2px solid #d1d5dc',
+          backgroundColor: selected === i ? 'white' : '#d1d5dc',
+        }"
+        v-for="(tab, i) in data"
+        :aria-selected="selected === i"
+        :aria-controls="tab.label + ' panel'"
+        :id="tab.label"
+      >
+        {{ tab.label }}
+      </button>
+    </div>
+    <div
       v-for="(tab, i) in data"
-      :aria-selected="selected === i"
-      :aria-controls="tab.label + ' panel'"
-      :id="tab.label"
+      role="tabpanel"
+      :id="tab.label + ' panel'"
+      class="px-10 py-5"
+      :style="{ display: selected !== i ? 'none' : 'block' }"
+      :aria-hidden="selected !== i"
+      :aria-labelledby="tab.label"
     >
-      {{ tab.label }}
-    </button>
-  </div>
-  <div
-    v-for="(tab, i) in data"
-    role="tabpanel"
-    :id="tab.label + ' panel'"
-    class="px-10 py-5"
-    :style="{ display: selected !== i ? 'none' : 'block' }"
-    :aria-hidden="selected !== i"
-    :aria-labelledby="tab.label"
-  >
-    {{ tab.content }}
+      {{ tab.content }}
+    </div>
   </div>
 </template>
